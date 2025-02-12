64ab691037a775725f5c4cb98d1355bc350d9136
Tiago <tjgfil@gmail.com>
2025-02-12 01:00:01 +0000

    Removed extra .js files

--- src/controllers/dtos/UserWalletInfoOutput.js
+++
@@ -1,9 +0,0 @@
-import { KRC20BalanceDTO } from "../../services/dtos/KRC20/KRC20BalanceDTO.ts";
-export class UserWalletInfoOutput {
-    address;
-    KRCBalances;
-    constructor(address, KRCBalances) {
-        this.address = address;
-        this.KRCBalances = KRCBalances;
-    }
-}
